fe903ace2fff5a1bd1fef28ca51f04e788da5e45
Nhật Vũ <nhatvu148@gmail.com>
2022-12-13 10:45:02 +0900

    add logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+mod database;
 mod router;
 mod routes;
-mod database;
+
+use std::net::SocketAddr;
 
 use router::create_routes;
 use sea_orm::Database;
@@ -8,8 +10,10 @@ use sea_orm::Database;
 pub async fn run(database_uri: &str) {
     let database = Database::connect(database_uri).await.unwrap();
     let app = create_routes(database).await;
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3001));
+    tracing::debug!("listening on {}", addr);
 
-    axum::Server::bind(&"0.0.0.0:3001".parse().unwrap())
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use axum_service::run;
 use dotenvy::dotenv;
 use dotenvy_macro::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
     dotenv().ok();
     let database_uri = dotenv!("DATABASE_URL");
 
